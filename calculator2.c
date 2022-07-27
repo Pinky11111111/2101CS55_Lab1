08b1f65dc481c95cf1c3d36971c3d07f1ac27ebb
Nunavath Varshini <99939960+Pinky11111111@users.noreply.github.com>
2022-07-27 19:46:39 +0530

    Add files via upload

---
+++ calculator2.c
@@ -0,0 +1,10 @@
+
+   switch (choice)
+   {
+   case 1:
+      printf("Enter two numbers: \n");
+      scanf("%ld %ld", &num1, &num2);
+      x = num1 + num2;
+      printf("Sum = %ld", x);
+      break;
+   
